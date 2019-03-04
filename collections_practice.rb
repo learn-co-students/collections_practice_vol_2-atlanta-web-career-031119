3e96e0d21272c4d65e217bfca400f04e0a71db55
Travis Martin <travisemerymartin@gmail.com>
2019-03-04 20:13:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,7 @@ def begins_with_r(array)
   if array.length>0
     i=0
     while i<array.length
-      if array[i][1] == 'r'
+      if array[i, 1] == 'r'
         i+=1
       else
         return false
