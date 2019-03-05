8ae48fb7792dd26d778839d62b2b9eaacd038340
favorable-vector-1743 <epzads@gmail.com>
2019-03-05 17:22:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,81 @@
 # your code goes here
+
+def begins_with_r(array)
+new_array = []
+  array. each do |name|
+
+    if name.start_with?("r")
+       new_array << "true"
+       
+    else 
+
+      new_array << "false"
+
+    end     
+     
+  end 
+  if new_array.include? "false"
+
+    return false 
+else 
+
+  return true 
+    
+end   
+end 
+
+
+def contain_a(array)
+new_array = []
+  array. each do |name|
+
+    if name.include?"a"
+       new_array << name
+    end     
+  end 
+return new_array 
+end 
+
+
+
+def first_wa(array)
+  new_array = []
+array.each do |name|
+
+   if name.start_with?("wa")
+    new_array << name 
+    break 
+   
+
+end 
+end 
+
+return new_array.join
+end
+
+def remove_non_strings(array)
+
+array.delete_if {|z| z.class != String }
+
+return array 
+
+  
+end
+
+def count_elements(array)
+counter = 0 
+array.uniq.each do |x| 
+ 
+array.each do |y|
+  
+  if y == x 
+     counter += 1 
+ end 
+ x[:count] = counter
+end 
+end 
+end 
+
+
+
+
